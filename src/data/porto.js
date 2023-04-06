b5afa510cd02041e0f61cda5b2d5e50b47b59ee9
azzamhfa <azzamuddienhanifa@gmail.com>
2023-04-06 17:26:29 +0700

    Updated

--- src/data/porto.js
+++ src/data/porto.js
@@ -1,28 +1,28 @@
-import { port1,port2, port3 } from "../assets"
+import { port1, port2, port3 } from "../assets";
 
 export default [
-    {
-        title: 'Company Website Template',
-        imgUrl: port1,
-        stack: ['ReactJS', 'Tailwind CSS'],
-        link: ""
-    },
-    {
-        title: 'PT Juara Website',
-        imgUrl: port2,
-        stack: ['Laravel 8', 'Bootstrap'],
-        link: ""
-    },
-    {
-        title: 'Analysis of Metaverse based on Youtube Comments',
-        imgUrl: '',
-        stack: ['Spreadsheet', 'Python'],
-        link: ""
-    },
-    {
-        title: 'Azzamhfa Portfolio',
-        imgUrl: {port3},
-        stack: ['ReactJS', 'Tailwind CSS'],
-        link: ""
-    }
-]
+  {
+    title: "Company Website Template",
+    imgUrl: port1,
+    stack: ["ReactJS", "Tailwind CSS"],
+    link: "",
+  },
+  {
+    title: "PT Juara Website",
+    imgUrl: port2,
+    stack: ["Laravel 8", "Bootstrap"],
+    link: "",
+  },
+  {
+    title: "Analysis of Metaverse based on Youtube Comments",
+    imgUrl: "",
+    stack: ["Spreadsheet", "Python"],
+    link: "",
+  },
+  {
+    title: "Azzamhfa Portfolio",
+    imgUrl: { port3 },
+    stack: ["ReactJS", "Tailwind CSS"],
+    link: "",
+  },
+];
